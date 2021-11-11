44331354c2c12d92cecff2412d0d808f9fe3190f
Guffroy Gregory <gregoryguffroypro@gmail.com>
2021-11-11 18:46:28 +0100

    [Ajout de tests unitaires et d'intégration] container/NewBills - Statemt
    Ajout de la partie de test concernant les statements

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,112 @@
-import { screen } from "@testing-library/dom"
+import { createEvent, fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
+import { ROUTES } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    describe("When I choose a wrong extension file", () => {
+      test("Then it should trigger on error message", () => {
+        window.alert = jest.fn()
+        // Définir user
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        //Définir onNavigate
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+
+        const html = NewBillUI()
+        document.body.innerHTML = html
+
+        const file = new File(['pdf'], 'test.pdf', {type: 'application/pdf'})
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firestore: null,
+          localStorage: window.localStorage
+        })
+        const inputFile = screen.getByTestId('file')
+
+        const handleChangeFile = jest.fn(newBill.handleChangeFile)
+
+        inputFile.addEventListener('input', handleChangeFile)
+        fireEvent(inputFile, createEvent('input', inputFile, {target: {files: file}}))
+
+        expect(handleChangeFile).toBeCalled()
+        expect(window.alert).toHaveBeenCalledWith('Veuillez inserer un justificatif au format .jpg/.jpeg/.png')
+      })
     })
+    describe("When I choose a good extension file", () => {
+      test("Then the file input should change", () => {
+        // Définir user
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        //Définir onNavigate
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+
+        const html = NewBillUI()
+        document.body.innerHTML = html
+
+        const file = new File(['test'], 'test.jpg', {type: 'image/jpg'})
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firestore: null,
+          localStorage: window.localStorage
+        })
+        const inputFile = screen.getByTestId('file')
+
+        const handleChangeFile = jest.fn(newBill.handleChangeFile)
+
+        inputFile.addEventListener('change', handleChangeFile)
+        fireEvent(inputFile, createEvent('change', inputFile, {target: {files: file}}))
+
+        expect(handleChangeFile).toBeCalled()
+        expect(inputFile.files.name).toBe('test.jpg');
+      })
+    })
+    describe("When I submit the form with an image", () => {
+      test("Then it should create a new bill and I should be redirected to the dashboard", () =>{
+        // Définir user
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        //Définir onNavigate
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const html = NewBillUI()
+        document.body.innerHTML = html
+        
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          firestore: null,
+          localStorage: window.localStorage
+        })
+
+        const submitButton = screen.getByTestId('form-new-bill');
+
+        const handleSubmit = jest.fn(newBill.handleSubmit)
+
+        submitButton.addEventListener('submit', handleSubmit)
+
+        fireEvent.submit(submitButton)
+
+        expect(handleSubmit).toBeCalled()
+        expect(screen.getAllByText('Mes notes de frais')).toBeTruthy()
+      })
+    })
+    
   })
 })
